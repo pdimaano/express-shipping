9f4c73fff16a0deeb935cf6bffa7fd483efa9f34
Spencer Taira <spencermtaira@gmail.com>
2022-11-08 17:37:47 -0800

    added mocking to tests and rithm code review comments

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,15 @@
 "use strict";
 
+const func /* const { shipProduct }*/ = require('../shipItApi.js') //TODO: change constant name
+func.shipProduct = jest.fn();
 const request = require("supertest");
 const app = require("../app");
 
 
 describe("POST /", function () {
   test("valid", async function () {
+    func.shipProduct.mockReturnValue(1234);
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,7 +17,7 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: expect.any(Number) }); //TODO: Test for expected number (1234)
   });
 
   test("throws error if empty request body", async function () {
--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,19 @@
 "use strict";
 
+const AxiosMockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
 const {
   shipProduct,
 } = require("./shipItApi");
 
 
 test("shipProduct", async function () {
+
+  axiosMock.onPost(`http://localhost:3001/ship`)
+    .reply(201, {receipt: {shipId: 3000}});
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +21,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(expect.any(Number)); //TODO: test for actual id 3000
 });
